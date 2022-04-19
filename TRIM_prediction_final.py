9ac0975e21debfbff648fc262a79925eab73c427
sharibhumayun <66083313+sharibhumayun@users.noreply.github.com>
2022-04-20 01:33:29 +0300

    Update TRIM_prediction_final.py

--- TRIM_prediction_final.py
+++ TRIM_prediction_final.py
@@ -9,6 +9,7 @@ import pickle
 import streamlit as st
 import numpy as np
 from urllib.request import urlopen
+!pip install cloudpickle
 import cloudpickle as cp
 
 loaded_model = cp.load(urlopen('https://github.com/sharibhumayun/predict-trim/blob/main/trained_model.pkl', 'rb'))
