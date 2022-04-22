6a47fb6f53dbb100d8b6496a4e24c0edf579ca2c
sharibhumayun <66083313+sharibhumayun@users.noreply.github.com>
2022-04-22 16:46:49 +0300

    Update TRIM_prediction_11.py

--- TRIM_prediction_11.py
+++ TRIM_prediction_11.py
@@ -12,9 +12,7 @@ import sklearn
 from sklearn.model_selection import train_test_split
 x_train,x_test,y_train,y_test = train_test_split(x,y,test_size = 0.2, random_state = 42)
 
-from sklearn.linear_model import LinearRegression
-lr = LinearRegression()
-lr.fit(x_train,y_train)
+
 
 from sklearn.ensemble import RandomForestRegressor
 rf = RandomForestRegressor()
