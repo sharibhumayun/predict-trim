8bd886d94b6217569106e674c8611c2edaed4d9f
sharibhumayun <66083313+sharibhumayun@users.noreply.github.com>
2022-04-24 12:21:40 +0300

    Update TRIM_prediction_11.py

--- TRIM_prediction_11.py
+++ TRIM_prediction_11.py
@@ -30,7 +30,7 @@ def TRIM_prediction(Input_values):
    
     Input_values = np.asarray(Input_values)
     Input_values = Input_values.reshape(1,-1)
-    y_pred1 = rf.predict(Input_values)
+    y_pred1 = lr.predict(Input_values)
     y_pred1
     
     
